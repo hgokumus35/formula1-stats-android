d2e27c63241c103ee45a8d8d971148a22d5b62e6
hgokumus35 <gokalp.okumus@topkapidanismanlik.com.tr>
2022-08-18 12:24:09 +0300

    project base

--- app/src/main/java/com/formula1/formula1stats/MainActivity.kt
+++ app/src/main/java/com/formula1/formula1stats/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.formula1.formula1stats
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.formula1.formula1stats.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listeners()
+    }
+
+    private fun listeners() {
+        binding.proceedToTheAppText.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MainPageActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/formula1/formula1stats/MainPageActivity.kt
@@ -0,0 +1,11 @@
+package com.formula1.formula1stats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
